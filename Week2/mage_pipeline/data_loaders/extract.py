470779be2f6af9a7f2892082f419a9a41928a10b
pnqthanh <thanhwtf95@gmail.com>
2024-10-27 14:50:37 +0700

    dockerize week 2

---
+++ Week2/mage_pipeline/data_loaders/extract.py
@@ -0,0 +1,31 @@
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+import pandas as pd
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+    prefix = kwargs['prefix']
+    suffix = kwargs['suffix']
+    data = pd.DataFrame()
+    for month in [10, 11, 12]:
+        url = f"{prefix}{suffix}{month}.csv.gz"
+        print(url)
+        data = pd.concat([data, pd.read_csv(url)])
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
